cf8b34f32944b895bdc9005200287979a6b1a148
Dattatray_Niture <nituredattatray1432@gmail.com>
2022-02-21 16:55:11 +0530

    nodejs first task completed

---
+++ src/routes/logger.js
@@ -0,0 +1,13 @@
+let url = 'https://www.google.com';//"room1"
+let room = "functionup room1";
+function logg(message){
+    console.log(message)
+}
+
+module.exports.url = url; // also we can write same name or different;
+module.exports.logg = logg;
+
+
+// just run this command in thorium folder
+// git checkout -f session/nodejs-modules
+
