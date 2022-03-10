b567c0de8ed80591d78fcf0a438031e7029518c3
Dattatray_Niture <nituredattatray1432@gmail.com>
2022-03-10 07:07:58 +0530

    jwt authorisation assignment-2 is done

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,99 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
-
-    next()
-}
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/userModel')
 
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
-}
+
+const jwtauth = async function(req,res,next){
+
+    try{
+    //authenticate
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    //If no token is present in the request header return error
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+  
+   // console.log(token);
+  
+    let decodedToken = jwt.verify(token, "functionup-thorium");
+    if (!decodedToken)
+      return res.send({ status: false, msg: "token is invalid" });
+
+
+
+    // authorise
+      
+    //userId for which the request is made. In this case message to be posted.
+    let userToBeModified = req.params.userId
+    console.log(userToBeModified)
+    //userId for the logged-in user
+    let userLoggedIn = decodedToken.userId
+    console.log(userLoggedIn)
+    //userId comparision to check if the logged-in user is requesting for their own data
+    if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+
+      next()
+    }catch(error){
+        return res.status(404).send(error.message)
+    } 
+  
+  }
+  
+  const userExist = async function(req,res,next){
+  
+      try{
+      let userId = req.params.userId;
+      if(!userId){return res.send({msg:"please give userId"})}
+    
+      let user = await userModel.findOne({_id:userId ,isDeleted:'false'});
+      //Return an error if no user with the given id exists in the db
+      if (!user) {
+        return res.send("No such user exists");
+      }
+      
+      next()
+    }catch(error){
+        res.status(200).send(error.message)
+    }
+  
+  }
+  
+  
+  
+  module.exports.userExist = userExist
+  module.exports.jwtauth = jwtauth
+  
+
+
+
+
+
+
+
+
+
+// const authenticate = function(req, req, next) {
+//     //check the token in request header
+//     //validate this token
+
+//     next()
+// }
+
+
+// const authorise = function(req, res, next) {
+//     // comapre the logged in user's id and the id in request
+//     next()
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const middlewares = require('../middleware/auth')
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
 router.post("/users", userController.createUser)
-
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
 router.get("/users/:userId", userController.getUserData)
-router.post("/users/:userId/posts", userController.postMessage)
+router.post("/users/:userId/posts",middlewares.jwtauth ,middlewares.userExist , userController.postMessage)
 
-router.put("/users/:userId", userController.updateUser)
-router.delete('/users/:userId', userController.deleteUser)
+router.put("/users/:userId",middlewares.jwtauth ,middlewares.userExist ,userController.updateUser)
+router.delete('/users/:userId',middlewares.jwtauth,middlewares.userExist,userController.deleteUser)
 
 module.exports = router;
+// 62272e46569c64af42cd093a
+//6228aae1f88f8878558782f3
